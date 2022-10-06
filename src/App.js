67c9c355f9e23d85c35b90bfb6d801cd71ffbf2a
armohsin <89929080+armohsin@users.noreply.github.com>
2022-10-06 10:41:38 +0500

    checks

--- src/App.js
+++ src/App.js
@@ -10,20 +10,69 @@ import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Button from '@mui/material/Button';
-
-
+import { Checkbox, MenuItem, Stack} from '@mui/material';
+import {useState} from "react";
 function App() {
+  const [message, setMessage] = useState('');
+  const [message2, setMessage2] = useState('');
+  const [ischecked, setIschecked] = useState(false);
+  const [isfield,setfield]=useState(true)
 
-  
+  const handleChange = event => {
+    const result = event.target.value.replace(/[^a-z]/gi, '');
+    setMessage(result);
+  };
 
+  const handleChange2 = event => {
+    const result2 = event.target.value.replace(/[^a-z]/gi, '');
+    setMessage2(result2);
+  };
+  const handleChange3 = event => {
+    if (event.target.checked) {
+      setfield(false)
+    } else {
+      console.log("unchecked")
+    }
+  };
   return (
     <>
      <CssBaseline />
-      <Container style={{padding:20}} >
-        
-      <TextField   id="outlined-basic" label="Student Name" variant="outlined"   />
-      <TextField  id="filled-basic" label="Father Name" variant="outlined"  style={{ marginLeft:20}} />
-       
+     <Container style={{padding:20}} >
+     <Box sx={{ bgcolor: '#cfe8fc', height: '800px', padding:'20px',borderRadius:'10px' }} >
+        <center>
+        <h2>University Form</h2>
+        </center>
+        <Stack direction="row" spacing={2} >
+      <TextField  id="outlined-basic" label="Student Name" variant="outlined"   required
+      type="text"
+        value={message}
+        onChange={handleChange}
+      />
+      <TextField  id="filled-basic" label="Father Name" variant="outlined" required
+      type="text"
+      value={message2}
+      onChange={handleChange2} />
+      </Stack>
+      <div style={{ display: 'flex'}}>
+
+      <FormControlLabel   control={<Checkbox defaultChecked />} label="Are you Pakistani"
+      type="checkbox"
+      value={ischecked}
+      onChange={handleChange3}
+      id="subscribe"
+      name="subscribe" />
+
+      {/* <label htmlFor="subscribe">
+
+        <input
+          type="checkbox"
+          value={ischecked}
+          onChange={handleChange3}
+          id="subscribe"
+          name="subscribe"
+        />
+        Subscribe
+      </label> */}
       <Autocomplete
       
       disablePortal
@@ -31,9 +80,10 @@ function App() {
       style={{marginTop:20}}
       options={top100Films}
       sx={{ width: 300 }}
-      renderInput={(params) => <TextField {...params} label="Nationality" />}
-    />
       
+      renderInput={(params) => <TextField {...params} label="Nationality" /> }
+    />
+      </div>
     <div style={{ display: 'flex'}}>
 
     <Autocomplete
@@ -43,10 +93,13 @@ function App() {
       options={cities}
       sx={{ width: 300 }}
       renderInput={(params) => <TextField {...params} label="City" />}
-
+      disabled={isfield}
     />
-     <TextField type="text" style={{marginTop:20, marginLeft:20}}  id="outlined-basic" label="Domicile" variant="outlined"   />
-    <TextField   type="number" style={{marginTop:20, marginLeft:20}} id="outlined-basic" label="CNIC" variant="outlined"   />
+     <TextField type={"text"} style={{marginTop:20, marginLeft:20}}  id="outlined-basic" label="Domicile" variant="outlined"  disabled={isfield}  />
+    <TextField  disabled={isfield} type="number" style={{marginTop:20, marginLeft:20}} id="outlined-basic" label="CNIC" variant="outlined"  helperText="Enter your 13 digit CNIC number" 
+     onInput = {(e) =>{
+      e.target.value = Math.max(0, parseInt(e.target.value) ).toString().slice(0,13)
+  }} />
     </div>
   < div style={{marginTop:20}}>
     <FormLabel  id="demo-controlled-radio-buttons-group">Gender</FormLabel>
@@ -54,12 +107,13 @@ function App() {
         aria-labelledby="demo-controlled-radio-buttons-group"
         name="controlled-radio-buttons-group"
       >
-        <FormControlLabel value="female" control={<Radio />} label="Female" />
-        <FormControlLabel value="male" control={<Radio />} label="Male" />
+        <FormControlLabel value="female" control={<Radio />} label="Female" style={{width:'100px'}} />
+        <FormControlLabel value="male" control={<Radio />} label="Male" style={{width:'100px'}} />
       </RadioGroup>
       </div >
 
       <div style={{ display: 'flex'}}>
+     
 
       <Autocomplete
       
@@ -70,7 +124,7 @@ function App() {
       sx={{ width: 300 }}
       renderInput={(params) => <TextField {...params} label="Select your campus" />}
     />
-
+    
 <Autocomplete
       
       disablePortal
@@ -98,13 +152,16 @@ function App() {
       renderInput={(params) => <TextField {...params} label="Select your FSC course" />}
     />   
 
-    <TextField type="number" style={{marginTop:20, marginLeft:20,width:150}}  id="outlined-basic" label="Marks" variant="outlined"   />
+    <TextField type="number" style={{marginTop:20, marginLeft:20,width:150}}  id="outlined-basic" label="Marks" variant="outlined"  
+       
+   />
     <TextField type="number" style={{marginTop:20, marginLeft:20,width:150}} id="outlined-basic" label="Percentage (%)" variant="outlined"   />
 
 
     </div>
-    <Button style={{marginTop:20}} variant="contained">Submit</Button>
-
+    <Button style={{marginTop:20}} type={"submit"} variant="contained">Submit</Button>
+    
+      </Box>
       </Container>
 
     </>
@@ -117,8 +174,7 @@ const top100Films = [
   { label: 'Australia' },
   { label: 'Bahrain' },
   { label: 'England' },
-  { label: "India" },
-  {  label: 'Pakistan' }];
+  { label: "India" }];
 
   const cities = [
     { label: 'Abottabad' },
